be72d867841523569d3aaecbdc2bc936e4fecbd6
lighteko <huijunyi0212@naver.com>
2026-02-28 23:48:52 +0900

    fix: pg types

--- lib/db.ts
+++ lib/db.ts
@@ -1,4 +1,4 @@
-import { Pool, type QueryResult } from "pg";
+import { Pool, type QueryResult, type QueryResultRow } from "pg";
 
 declare global {
   // eslint-disable-next-line no-var
@@ -75,7 +75,7 @@ function getPool(): Pool {
   return pool;
 }
 
-export async function query<T = unknown>(
+export async function query<T extends QueryResultRow = QueryResultRow>(
   text: string,
   params?: unknown[]
 ): Promise<QueryResult<T>> {
